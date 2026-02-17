d8288f24d47ef84c98585d2f0f52d6e46c0aa798
jashwanthdevops <jashwanthnaredla@gmail.com>
2026-02-18 00:38:00 +0530

    2nd commit

--- resource.tf
+++ resource.tf
@@ -8,7 +8,14 @@ resource "aws_instance" "web_public" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   key_name = "nexus"
-
+    user_data = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y nginx1
+systemctl enable nginx
+systemctl start nginx
+echo "<h1>Welcome to vcube! AWS infra created using Terraform</h1>" > /usr/share/nginx/html/index.html
+EOF
   tags = {
     Name = "web-${count.index}"
   }
